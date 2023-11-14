498bc47cfea71ea07cbbbaed86523c85ab768cd1
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-11-14 22:08:41 +0530

    Text Decoration Website Project Comleted

--- textoperations/textoperations/views.py
+++ textoperations/textoperations/views.py
@@ -21,16 +21,63 @@ def relation(request):
 
 def task(request):
     # get the text
-    djtext = request.GET.get('prompt','default')
-    rpunc = request.GET.get('removepunc','off')
+    e = 0
+    djtext = request.POST.get('prompt','default')
+    data = djtext
+    rpunc = request.POST.get('removepunc','off')
     if rpunc == 'on':
+        e += 1
         result = ''
-        punclist = '''"[]{\}/?.,:;'-+%&^*()_-=$#@!~`<>'''
+        punclist = '''"[]{\}/?.,:;'-+%&^*()_-=$#@!~”₹|`<>'''
         for i in djtext:
             if i not in punclist:
                 result = result + i
-        newtext = {'task':'Remove Punctuation','result':result}
-        return render(request, "task.html", newtext)
+        djtext = result
 
-    return HttpResponse("Please Select Task Punctuation")
+    extraspace = request.POST.get('extraspace','off')
+    if extraspace == 'on':
+        e += 1
+        result = ''
+        for i,item in enumerate(djtext):
+            if item != ' ' and djtext[i+1] != ' ':
+                result = result + item
+                if i==len(djtext)-2:
+                    if djtext[-1]!=' ':
+                        result = result + djtext[-1]
+                    break
+        djtext = result
+
+    removenewline = request.POST.get('removenewline','off')
+    if removenewline == 'on':
+        e += 1
+        result = ''
+        print(djtext)
+        for i in djtext:
+            if i != '\n' and i != '\r':
+                result = result + i
+        djtext = result
+
+    text_format = request.POST.get('text-format','off')
+    if text_format == 'capital':
+        e += 1
+        djtext = djtext.capitalize()
+
+    elif text_format == 'lower':
+        e += 1
+        djtext = djtext.lower()
+
+    elif text_format == 'upper':
+        e += 1
+        djtext = djtext.upper()
+
+    elif text_format == 'title':
+        e += 1
+        djtext = djtext.title()
+
+    if e:
+        newtext = {'data':data,'result':djtext}
+        return render(request, 'task.html', newtext)
+
+    else:
+        return HttpResponse("Please Select Task Punctuation")
 
